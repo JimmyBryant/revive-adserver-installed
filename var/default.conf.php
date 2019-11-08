39eb008180476740945de52f3827b68c1c7edc3d
bitterg <lovekugua@gmail.com>
2019-11-08 11:09:17 +0800

    first commit

---
+++ var/default.conf.php
@@ -0,0 +1,3 @@
+;<?php exit; ?>
+;*** DO NOT REMOVE THE LINE ABOVE ***
+realConfig="47.98.145.165"
---
+++ var/templates_compiled/%%8D^8D7^8D794F90%%content-footer.html.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2019-11-08 02:59:23
+         compiled from layout/content-footer.html */ ?>
+<?php if ($this->_tpl_vars['showContentFrame']): ?>
+</div>
+<?php endif; ?>
